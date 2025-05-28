835d158551b13e3e55fe926e53f30037deb4a66d
oMegaTraiin <brunobezerra_bb@hotmail.com>
2025-05-28 20:14:14 -0300

    Update soma.test.js

--- test/soma.test.js
+++ test/soma.test.js
@@ -3,3 +3,11 @@ const soma = require('../src/soma');
 test('A soma de 1 + 3 é igual a 4', () => {
     expect(soma(1,3)).toBe(4);
 });
+
+test('A soma de 5 + 3 é igual a 8', () => {
+    expect(soma(5,3)).toBe(8);
+});
+
+test('A soma de 5 + 5 é igual a 10', () => {
+    expect(soma(5,3)).toBe(8);
+});
